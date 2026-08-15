7069eb100222b0cf1210ecfd7c375b02417cb4e0
Paul McKay <paulchangmckay@gmail.com>
2026-08-15 19:08:38 -0400

    feat: wire Teams/TeamPage/TopPlayers into the route table (#116, #117)

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,9 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import App from "./App";
 import Home from "./pages/Home";
 import Players from "./pages/Players";
+import Teams from "./pages/Teams";
+import TeamPage from "./pages/TeamPage";
+import TopPlayers from "./pages/TopPlayers";
 import PlaceholderPage from "./pages/PlaceholderPage";
 import "./index.css";
 
@@ -14,8 +17,9 @@ createRoot(document.getElementById("root")!).render(
         <Route element={<App />}>
           <Route index element={<Home />} />
           <Route path="players" element={<Players />} />
-          <Route path="teams" element={<PlaceholderPage title="Teams" />} />
-          <Route path="top-players" element={<PlaceholderPage title="Top Players" />} />
+          <Route path="teams" element={<Teams />} />
+          <Route path="teams/:teamId" element={<TeamPage />} />
+          <Route path="top-players" element={<TopPlayers />} />
           <Route path="betting" element={<PlaceholderPage title="Betting" />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Route>
